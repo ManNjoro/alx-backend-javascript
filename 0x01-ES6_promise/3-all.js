e7abb477cdf2ac24e454ec2246ca1db04ed29436
ManNjoro <elijohnmwoho@gmail.com>
2023-11-29 23:49:45 +0300

    debug 3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  // Use Promise.all to wait for both promises to settle
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => {
-      // Handle errors from any of the promises
       console.log('Signup system offline');
     });
 }
