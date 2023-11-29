2146291a3b24c5fee9b1675b91639500c5e2a036
ManNjoro <elijohnmwoho@gmail.com>
2023-11-29 23:47:55 +0300

    debug 3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,7 @@ export default function handleProfileSignup() {
   // Use Promise.all to wait for both promises to settle
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [photoRes, userRes] = results;
-      console.log(photoRes.body, userRes.firstName, userRes.lastName);
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => {
       // Handle errors from any of the promises
