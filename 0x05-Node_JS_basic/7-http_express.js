efcdf29dacf5522ef0c668f91cacfccd518c5146
ManNjoro <elijohnmwoho@gmail.com>
2024-01-30 20:03:44 +0300

    Refactor code to use promises and async/await.

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,74 +1,91 @@
-const fs = require('fs').promises;
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-const port = 1245;
+const PORT = 1245;
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  fs.readFile(dataPath, 'utf-8')
-    .then((fileContent) => {
-      const fileLines = fileContent.trim().split('\n');
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const reportParts = [];
+        const fileLines = data.toString('utf-8').trim().split('\n');
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(',');
+        const studentPropNames = dbFieldNames.slice(
+          0,
+          dbFieldNames.length - 1,
+        );
 
-      const studentGroups = {};
-      const dbFieldNames = fileLines[0].split(',').slice(0, -1);
-
-      for (const line of fileLines.slice(1)) {
-        const studentRecord = line.split(',');
-        const studentPropValues = studentRecord.slice(0, -1);
-        const field = studentRecord.slice(-1)[0];
-
-        if (!studentGroups[field]) {
-          studentGroups[field] = [];
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord.slice(
+            0,
+            studentRecord.length - 1,
+          );
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+          const studentEntries = studentPropNames.map((propName, idx) => [
+            propName,
+            studentPropValues[idx],
+          ]);
+          studentGroups[field].push(Object.fromEntries(studentEntries));
         }
 
-        const studentEntry = Object.fromEntries(
-          dbFieldNames.map((propName, idx) => [propName, studentPropValues[idx]]),
+        const totalStudents = Object.values(studentGroups).reduce(
+          (pre, cur) => (pre || []).length + cur.length,
         );
-        studentGroups[field].push(studentEntry);
+        reportParts.push(`Number of students: ${totalStudents}`);
+        for (const [field, group] of Object.entries(studentGroups)) {
+          reportParts.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        resolve(reportParts.join('\n'));
       }
-
-      const totalStudents = Object.values(
-        studentGroups,
-      ).reduce((acc, group) => acc + group.length, 0);
-
-      const result = {
-        totalStudents,
-        studentGroups,
-      };
-
-      resolve(result);
-    })
-    .catch(() => {
-      reject(new Error('Cannot load the database'));
     });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
-
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  const databasePath = process.argv[2];
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
 
-  countStudents(databasePath)
-    .then((result) => {
-      const { totalStudents, studentGroups } = result;
-      let response = `This is the list of our students\nNumber of students: ${totalStudents}\n`;
-
-      for (const [field, group] of Object.entries(studentGroups)) {
-        const studentNames = group.map((student) => student.firstname).join(', ');
-        response += `Number of students in ${field}: ${group.length}. List: ${studentNames}\n`;
-      }
-
-      res.send(response);
+  countStudents(DB_FILE)
+    .then((report) => {
+      responseParts.push(report);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
     })
-    .catch((error) => {
-      res.status(500).send(`Error: ${error.message}`);
+    .catch((err) => {
+      responseParts.push(err instanceof Error ? err.message : err.toString());
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
     });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
