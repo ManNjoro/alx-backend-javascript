fa66c5cbe73d533184fcf8b41fa77f7f24caeb76
ManNjoro <elijohnmwoho@gmail.com>
2024-02-01 19:19:55 +0300

    Spy on `calculateNumber` instead of `Utils` in `sendPaymentRequestToApi` test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,14 +5,18 @@
 const sinon = require('sinon');
 const chai = require('chai');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
 const { expect } = chai;
 
 describe('sendPaymentRequestToApi', () => {
   it('sum', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
+    const spy = sinon.spy(console);
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.log.calledWith('The total is: 10')).to.be.true;
+    expect(spy.log.callCount).to.be.equal(1);
   });
 });
